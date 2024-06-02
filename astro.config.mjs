0355615e540c6a797263d8355581f6468dbf6a80
emgixiii <emgi@live.in>
2024-06-02 16:02:58 +0530

    Add Pretteir Configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-site: "https://emgi13.github.io",
-integrations: [mdx(), sitemap()],
+  site: "https://emgi13.github.io",
+  integrations: [mdx(), sitemap()],
 });
