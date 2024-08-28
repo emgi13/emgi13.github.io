13b0fe31619a289321d380146be7b72e1b72af7d
emgixiii <emgi@live.in>
2024-08-28 11:58:38 +0530

    FIX header with logo

---
+++ src/components/SiteLogo/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { SITE_TITLE } from "../../consts";
+import { LOGO_SMALL } from "../../assets/logos";
+import "./SiteLogo.scss";
+
+const SiteLogo = () => {
+  return (
+    <div className="SiteLogo">
+      <LOGO_SMALL />
+      <span>{SITE_TITLE}</span>
+    </div>
+  );
+};
+
+export default SiteLogo;
