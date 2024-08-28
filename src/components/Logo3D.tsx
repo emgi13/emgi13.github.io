613fbbaef6ed8b7de35a75270c88bfe760f10de1
emgixiii <emgi@live.in>
2024-08-28 10:59:24 +0530

    REF changed logo3d to functional component

--- src/components/Logo3D.tsx
+++ src/components/Logo3D.tsx
@@ -5,34 +5,31 @@ import "./Logo3D.scss";
 
 let key_id = 0;
 
-class Logo3D extends Component {
-  render() {
-    const repeat = 3;
-    const start = -5;
-    const diff = +1;
-    return (
-      <>
-        <Atropos className="logo-3d-atropos" shadow={false}>
-          {LOGO_LAYERS.reverse().map((v, i) => {
-            let divs = [];
-            for (let ind = 0; ind < repeat; ind++) {
-              divs.push(
-                <div
-                  className="logo-layer"
-                  data-atropos-offset={start + i * diff + (diff * ind) / repeat}
-                  key={"3d-logo-layer-" + key_id++}
-                >
-                  {v}
-                </div>,
-              );
-            }
-            return divs;
-          })}
-        </Atropos>
-        {/* {LOGO_LAYERS.map((v) => v)} */}
-      </>
-    );
-  }
-}
+const Logo3D = () => {
+  const repeat = 3;
+  const start = -5;
+  const diff = 1;
+  return (
+    <>
+      <Atropos className="logo-3d-atropos" shadow={false}>
+        {LOGO_LAYERS.reverse().map((v, i) => {
+          let divs = [];
+          for (let ind = 0; ind < repeat; ind++) {
+            divs.push(
+              <div
+                className="logo-layer"
+                data-atropos-offset={start + i * diff + (diff * ind) / repeat}
+                key={"3d-logo-layer-" + key_id++}
+              >
+                {v}
+              </div>,
+            );
+          }
+          return divs;
+        })}
+      </Atropos>
+    </>
+  );
+};
 
 export default Logo3D;
