2a4af922b5ff493e7c01bccd33a196a8b50a0494
emgixiii <emgi@live.in>
2024-08-28 10:41:15 +0530

    FIX 3D logo

--- src/assets/logos.tsx
+++ src/assets/logos.tsx
@@ -6,6 +6,9 @@ export const LOGO_SMALL = () => (
     viewBox="0 -0.5 13 13"
     shape-rendering="crispEdges"
   >
+    <metadata>
+      Made with Pixels to Svg https://codepen.io/shshaw/pen/XbxvNj
+    </metadata>
     <path
       stroke="currentColor"
       d="M0 0h13M0 1h1M4 1h3M10 1h3M0 2h1M2 2h1M4 2h3M8 2h1M10 2h3M0 3h1M2 3h1M8 3h1M10 3h3M0 4h1M2 4h1M4 4h1M6 4h1M8 4h1M12 4h1M0 5h1M2 5h1M4 5h1M6 5h1M8 5h1M10 5h1M12 5h1M0 6h1M2 6h1M4 6h1M6 6h1M8 6h1M10 6h1M12 6h1M0 7h1M2 7h1M4 7h1M6 7h1M8 7h1M10 7h1M12 7h1M0 8h1M10 8h1M12 8h1M0 9h1M9 9h1M12 9h1M0 10h1M2 10h7M11 10h2M0 11h1M10 11h3M0 12h13"
@@ -19,6 +22,9 @@ export const LOGO_FULL = () => (
     viewBox="0 -0.5 17 30"
     shape-rendering="crispEdges"
   >
+    <metadata>
+      Made with Pixels to Svg https://codepen.io/shshaw/pen/XbxvNj
+    </metadata>
     <path
       stroke="currentColor"
       d="M0 0h17M0 1h1M16 1h1M0 2h1M2 2h13M16 2h1M0 3h1M2 3h1M6 3h3M12 3h3M16 3h1M0 4h1M2 4h1M4 4h1M6 4h3M10 4h1M12 4h3M16 4h1M0 5h1M2 5h1M4 5h1M10 5h1M12 5h3M16 5h1M0 6h1M2 6h1M4 6h1M6 6h1M8 6h1M10 6h1M14 6h1M16 6h1M0 7h1M2 7h1M4 7h1M6 7h1M8 7h1M10 7h1M12 7h1M14 7h1M16 7h1M0 8h1M2 8h1M4 8h1M6 8h1M8 8h1M10 8h1M12 8h1M14 8h1M16 8h1M0 9h1M2 9h1M4 9h1M6 9h1M8 9h1M10 9h1M12 9h1M14 9h1M16 9h1M0 10h1M2 10h1M12 10h1M14 10h1M16 10h1M0 11h1M2 11h1M11 11h1M14 11h1M16 11h1M0 12h1M2 12h1M4 12h7M13 12h2M16 12h1M0 13h1M2 13h1M12 13h3M16 13h1M0 14h1M2 14h13M16 14h1M0 15h1M16 15h1M0 16h1M2 16h3M10 16h3M14 16h1M16 16h1M0 17h1M2 17h1M10 17h1M16 17h1M0 18h1M2 18h2M6 18h3M10 18h1M12 18h1M14 18h1M16 18h1M0 19h1M2 19h1M6 19h3M10 19h1M12 19h1M14 19h1M16 19h1M0 20h1M2 20h3M6 20h1M8 20h1M10 20h3M14 20h1M16 20h1M0 21h1M16 21h1M0 22h1M2 22h6M9 22h8M0 23h1M2 23h5M13 23h4M0 24h1M2 24h2M8 24h4M13 24h4M0 25h1M2 25h2M8 25h1M11 25h1M16 25h1M0 26h1M2 26h4M8 26h1M11 26h1M13 26h2M16 26h1M0 27h1M2 27h5M8 27h4M13 27h2M16 27h1M0 28h1M16 28h1M0 29h17"
@@ -32,6 +38,9 @@ export const LOGO_LAYERS = [
     viewBox="0 -0.5 17 30"
     shape-rendering="crispEdges"
   >
+    <metadata>
+      Made with Pixels to Svg https://codepen.io/shshaw/pen/XbxvNj
+    </metadata>
     <path
       stroke="currentColor"
       d="M6 6h1M8 6h1M6 7h1M8 7h1M6 8h1M8 8h1M6 9h1M8 9h1"
@@ -58,6 +67,10 @@ export const LOGO_LAYERS = [
     <metadata>
       Made with Pixels to Svg https://codepen.io/shshaw/pen/XbxvNj
     </metadata>
+    <path
+      stroke="currentColor"
+      d="M12 7h1M12 8h1M12 9h1M12 10h1M11 11h1M4 12h7"
+    />
   </svg>,
   <svg
     xmlns="http://www.w3.org/2000/svg"
@@ -146,8 +159,8 @@ export const LOGO_LAYERS = [
 //     <metadata>
 //       Made with Pixels to Svg https://codepen.io/shshaw/pen/XbxvNj
 //     </metadata>
-//     <path stroke="currentColor"
-//       stroke="#000000"
+//     <path
+//       stroke="currentColor"
 //       d="M0 0h17M0 1h17M0 2h17M0 3h17M0 4h17M0 5h17M0 6h17M0 7h17M0 8h17M0 9h17M0 10h17M0 11h17M0 12h17M0 13h17M0 14h17M0 15h17M0 16h17M0 17h17M0 18h17M0 19h17M0 20h17M0 21h17M0 22h17M0 23h17M0 24h17M0 25h17M0 26h17M0 27h17M0 28h17M0 29h17"
 //     />
 //   </svg>,
