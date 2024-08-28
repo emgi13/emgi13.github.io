f0284698969a84e0c0c055836dc3e85f711679eb
emgixiii <emgi@live.in>
2024-08-28 23:27:39 +0530

    FIX working p5js component in mdx (use a .astro component wrapper)

---
+++ src/components/GameOfLife/GameOfLifeComponent.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import p5 from "p5";
+
+class GameOfLifeComponent extends React.Component {
+  p5ref: React.RefObject<HTMLDivElement>;
+  p5: p5 | undefined;
+  constructor(props) {
+    super(props);
+    this.p5ref = React.createRef();
+  }
+  render() {
+    return <div ref={this.p5ref}></div>;
+  }
+
+  componentDidMount(): void {
+    this.p5 = new p5(this.sketch, this.p5ref.current as HTMLElement);
+  }
+
+  componentWillUnmount(): void {
+    if (this.p5) {
+      this.p5.remove();
+    }
+  }
+
+  sketch = (p: p5) => {
+    p.setup = () => {
+      p.createCanvas(400, 400);
+      p.background(220);
+    };
+
+    p.draw = () => {
+      if (p.mouseIsPressed) {
+        p.stroke(0);
+        p.line(p.mouseX, p.mouseY, p.pmouseX, p.pmouseY);
+      }
+    };
+  };
+}
+
+export default GameOfLifeComponent;
--- src/components/GameOfLife/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-class GameOfLife extends React.Component {
-  render() {
-    return <div>TEST</div>;
-  }
-}
-
-export default GameOfLife;
