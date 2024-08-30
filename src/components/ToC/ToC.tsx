80e79f8828439058c816df308e523863701a2e94
emgixiii <emgi@live.in>
2024-08-30 18:10:51 +0530

    FIX sticky with padding

--- src/components/ToC/ToC.tsx
+++ src/components/ToC/ToC.tsx
@@ -14,20 +14,39 @@ class ToC extends React.Component<{}, ToCState> {
       open: false,
     };
   }
+
+  get headings() {
+    const prose = document.querySelector(".prose")!;
+    const targets = prose.querySelectorAll("h1, h2, h3");
+    const headings = Array.from(targets)
+      .map((h) => ({
+        id: h.id,
+        text: h.textContent,
+        level: parseInt(h.tagName.replace("H", ""), 10),
+      }))
+      .filter((h) => !!h.id);
+    return headings;
+  }
   render() {
     const { open } = this.state;
     return (
       <div className="ToC">
-        <div className={"links-cont " + (open ? "" : "closed")}>
-          <div className="links">test</div>
+        <div className="sticker">
+          <div className={"links-cont " + (open ? "" : "closed")}>
+            <div className="links">
+              {this.headings.map((h) => (
+                <a href={"#" + h.id}>{h.text}</a>
+              ))}
+            </div>
+          </div>
+          <button
+            className="toggle"
+            onClick={() => this.setState({ open: !open })}
+          >
+            <Toc />
+            <span>Table of Contents</span>
+          </button>
         </div>
-        <button
-          className="toggle"
-          onClick={() => this.setState({ open: !open })}
-        >
-          <Toc />
-          <span>Table of Contents</span>
-        </button>
       </div>
     );
   }
