d176499f51f42303ea41e39029e8a8dbbceb05d4
emgixiii <emgi@live.in>
2024-08-30 10:24:51 +0530

    ADD rehype for autolinks

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,20 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import react from "@astrojs/react";
+import rehypeSlug from "rehype-slug";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://emgi13.github.io",
-  integrations: [mdx(), sitemap(), react()],
+  integrations: [
+    mdx({
+      rehypePlugins: [
+        rehypeSlug,
+        [rehypeAutolinkHeadings, { behavior: "wrap" }],
+      ],
+    }),
+    sitemap(),
+    react(),
+  ],
 });
