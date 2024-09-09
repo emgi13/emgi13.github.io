f0d4f33e383aa9851b583f1b388f129bb635984c
emgixiii <emgi@live.in>
2024-09-09 12:36:29 +0530

    ADD image maker

--- src/components/TuringPattern/utils.ts
+++ src/components/TuringPattern/utils.ts
@@ -1,3 +1,5 @@
+import type p5 from "p5";
+
 // INFO: get Periodic Boundary Conditions
 export function getPBC(grid: number[][], i: number, j: number): number {
   const width = grid.length;
@@ -45,4 +47,52 @@ export function Laplace(
   const sum =
     getter(i - i, j) + getter(i + 1, j) + getter(i, j - 1) + getter(i, j + 1);
   return (sum - 4 * getter(i, j)) / (dx * dx);
+}
+
+export function findMinMax(
+  matrix: number[][],
+): { min: number; max: number } | null {
+  if (matrix.length === 0 || matrix[0].length === 0) {
+    return null; // Return null if the input is an empty array
+  }
+
+  let min = Infinity;
+  let max = -Infinity;
+
+  for (const row of matrix) {
+    for (const num of row) {
+      if (num < min) {
+        min = num;
+      }
+      if (num > max) {
+        max = num;
+      }
+    }
+  }
+
+  return { min, max };
+}
+
+export function makeImage(p: p5, grid: number[][], size: number): p5.Image {
+  // Create an image object
+  let img = p.createImage(size, size);
+  img.loadPixels();
+
+  // get min max values
+  const { min, max } = findMinMax(grid)!;
+
+  // Set pixel values based on the 2D array
+  for (let i = 0; i < size; i++) {
+    for (let j = 0; j < size; j++) {
+      let brightness = grid[i][j];
+      let index = (j + i * size) * 4; // Calculate pixel index
+      img.pixels[index] = brightness; // Red
+      img.pixels[index + 1] = brightness; // Green
+      img.pixels[index + 2] = brightness; // Blue
+      img.pixels[index + 3] = 255; // Alpha
+    }
+  }
+
+  img.updatePixels(); // Update the image with new pixel data
+  return img;
 }
