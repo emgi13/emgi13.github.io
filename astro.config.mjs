4b0db557d21a56b58ff722e36884279e95a656f7
emgixiii <emgi@live.in>
2024-09-20 15:43:26 +0530

    FIX disable unused rehype

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,18 +2,18 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import react from "@astrojs/react";
-import rehypeSlug from "rehype-slug";
-import rehypeAutolinkHeadings from "rehype-autolink-headings";
+// import rehypeSlug from "rehype-slug";
+// import rehypeAutolinkHeadings from "rehype-autolink-headings";
 
 // https://astro.build/config
 export default defineConfig({
   site: "http://emgi.co.in",
   integrations: [
     mdx({
-      rehypePlugins: [
-        rehypeSlug,
-        [rehypeAutolinkHeadings, { behavior: "wrap" }],
-      ],
+      // rehypePlugins: [
+      //   rehypeSlug,
+      //   [rehypeAutolinkHeadings, { behavior: "wrap" }],
+      // ],
     }),
     sitemap(),
     react(),
